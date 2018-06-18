128fd2392b5a1cef0082abe1338dcf28b90a23a6
Mark Hayes <mark.m.hayes1@gmail.com>
2018-06-18 14:54:38 -0400

    adding few files to the structure to allow for cleaner sparcing of the css code. also created the footer nav and main navigation sections.

--- admin/wp-content/themes/advancesolutions/footer.php
+++ admin/wp-content/themes/advancesolutions/footer.php
@@ -15,16 +15,19 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'advancesolutions' ) ); ?>">
+            <?php wp_nav_menu( array( 'theme_location' => 'menu-2' ) ); ?>
+            <hr>
+            <?php wp_nav_menu( array( 'theme_location' => 'menu-3' ) ); ?>
+			<!-- <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'advancesolutions' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'advancesolutions' ), 'WordPress' );
 				?>
-			</a>
+			</a> -->
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'advancesolutions' ), 'advancesolutions', '<a href="http://potpie.digital/">Mark Hayes</a>' );
+				printf( esc_html__( 'Theme: %1$s by %2$s.', 'advancesolutions' ), 'advancesolutions', '<a href="http://potpie.digital/">Pot Pie Digital, LLC</a>' );
 				?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
