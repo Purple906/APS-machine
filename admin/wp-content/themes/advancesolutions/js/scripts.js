de36df1b1ff720422a00dfdb0d1d78efb57f0a15
Mark Hayes <mark.m.hayes1@gmail.com>
2018-06-28 12:19:52 -0400

    adding breakpoints to the carousel javascript so its mobile frinedly.

--- admin/wp-content/themes/advancesolutions/js/scripts.js
+++ admin/wp-content/themes/advancesolutions/js/scripts.js
@@ -10,13 +10,36 @@ var mySwiper = new Swiper ('.swiper-container', {
     direction: 'horizontal',
     loop: true,
     slidesPerView: 3,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     speed: 600,
         parallax: true,
-    spaceBetween: 0,
+    spaceBetween: 30,
 
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
+    },
+    breakpoints: {
+      // 1024: {
+      //   slidesPerView: 4,
+      //   spaceBetween: 40,
+      // },
+      // 768: {
+      //   slidesPerView: 3,
+      //   spaceBetween: 30,
+      // },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      400: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      }
     },
 
     navigation: {
