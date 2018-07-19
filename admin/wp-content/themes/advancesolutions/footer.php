a43bd554bd71c2d710bcc579eef0fef88bf089e8
Mark Hayes <mark.m.hayes1@gmail.com>
2018-07-19 15:04:00 -0400

    adjusting the footer some more to make it mobile friendly and add in a new background image. Still needs work as the logo gets cut off some how at tablet size.

--- admin/wp-content/themes/advancesolutions/footer.php
+++ admin/wp-content/themes/advancesolutions/footer.php
@@ -16,7 +16,7 @@
 	<footer id="colophon" class="site-footer">
         <div class="footer-content">
 		<div class="site-info">
-        <img src="<?php echo get_template_directory_uri();?>/assets/APS_logo_sm.png">
+        <!-- <img src="<?php echo get_template_directory_uri();?>/assets/APS_logo_sm.png"> -->
 		<div class="footer-navs">
             <?php wp_nav_menu( array( 'theme_location' => 'menu-2' ) ); ?>
             <nav class="menu-social-media-container">
